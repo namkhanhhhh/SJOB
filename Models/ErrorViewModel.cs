0b210c448d0714247482d73ce52a4cb801e5a968
namkhanhhhh <khanhnguyennam04@gmail.com>
2025-04-14 14:45:09 +0700

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SJOB_EXE201.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
